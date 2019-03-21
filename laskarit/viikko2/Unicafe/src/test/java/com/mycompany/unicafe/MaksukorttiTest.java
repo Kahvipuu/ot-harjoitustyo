6472d23b4fbee69fab4154e941d3477bb6103664
Kahvipuu <partaar@hotmail.com>
2019-03-21 13:15:44 +0200

    Testit maksukortille tehty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,48 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(1595);
+//rahaa on kortilla 1605 senttiä
+        assertEquals("saldo: 16.5", kortti.toString());
+//        sentit ilman kymmeniä käsitellään tooStringissä väärin
+    }
+
+    @Test
+    public void saldoVaheneeOikeinKunRahaRiittaa() {
+        kortti.lataaRahaa(1595);
+//rahaa on kortilla 1605 senttiä
+        kortti.otaRahaa(1505);
+        assertEquals("saldo: 1.0", kortti.toString());
+    }
+
+    @Test
+    public void saldoEiVaheneKunRahaEiRiita() {
+//rahaa on kortilla 10 senttiä
+        kortti.otaRahaa(1505);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueKunRahaRiittaa() {
+        kortti.lataaRahaa(1595);
+//rahaa on kortilla 1605 senttiä
+        assertTrue(kortti.otaRahaa(1505));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalseKunRahaEiRiitta() {
+//rahaa on kortilla 10 senttiä
+        assertFalse(kortti.otaRahaa(1505));
+    }
+
 }
